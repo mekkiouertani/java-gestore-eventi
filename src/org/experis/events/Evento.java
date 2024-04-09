81e83453944d34dec67dd03eb7ad2352e1639767
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2024-04-09 10:14:32 +0200

    MILESTONE 1 completed, method added for cancelling, booking, date formatting and exceptions

--- src/org/experis/events/Evento.java
+++ src/org/experis/events/Evento.java
@@ -1,6 +1,7 @@
 package org.experis.events;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Evento {
     private String titolo;
@@ -8,14 +9,26 @@ public class Evento {
     private int nPostiTotali;
     private int nPostiPrenotati;
 
-
+    //costruttore
     public Evento(String titolo, LocalDate data, int nPostiTotali) {
+
+        //eccezione se la data è una data del passato
+        if(data.isBefore(LocalDate.now())){
+            throw new IllegalArgumentException("La data dell'evento non può essere passata.");
+        }
+
+        //eccezione se i PostiTotali sono uguali o inferiori a 0
+        if(nPostiTotali <= 0){
+            throw new IllegalArgumentException("Il numero di posti totali deve essere positivo.");
+        }
+
         this.titolo = titolo;
         this.data = data;
         this.nPostiTotali = nPostiTotali;
-        this.nPostiPrenotati = 0;
+        this.nPostiPrenotati = 0; //PostiPrenotati inizializzati a 0
     }
 
+    //metodi Setter e Getter
     public String getTitolo() {
         return titolo;
     }
@@ -41,22 +54,29 @@ public class Evento {
     }
 
 
-  public void prenota(int nPosti){
+    //funzioni PRENOTA e DISDICI
 
-  }
+    //PRENOTA -> se la data è già passata o non ci sono abbastanza posti disponibili -> ECCEZIONE
+    public void prenota(int nPosti){
+        if(data.isBefore(LocalDate.now()) || (nPostiTotali - nPostiPrenotati) < nPosti){
+            throw new IllegalArgumentException("Non è possibile effettuare la prenotazione");
+        }
+        nPostiPrenotati += nPosti; // aggiunge N posti prenotati
+    }
 
-  public void disdici (int nPosti){
+    //DISDICI -> se la data è già passata o non ci sono abbastanza prenotazioni -> ECCEZIONE
+    public void disdici(int nPosti){
+        if(data.isBefore(LocalDate.now()) || nPostiPrenotati < nPosti ){
+            throw new IllegalArgumentException("Non è stato possibile disdire la prenotazione");
+        }
+        nPostiPrenotati -= nPosti; // riduce di N i posti prenotati
+    }
 
-  }
 
-
+    //Metodo toString per formattare la data
     @Override
     public String toString() {
-        return "Evento{" +
-                "titolo='" + titolo + '\'' +
-                ", data=" + data +
-                ", nPostiTotali=" + nPostiTotali +
-                ", nPostiPrenotati=" + nPostiPrenotati +
-                '}';
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return data.format(dateFormatter) + " - " + titolo;
     }
 }
